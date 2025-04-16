e69ee44933131019673176ef4e11ef05e29efc0d
ibrahimgamill <121096964+ibrahimgamill@users.noreply.github.com>
2025-04-16 21:39:14 +0200

    Implemented Customer

--- src/main/java/com/example/miniapp/models/Customer.java
+++ src/main/java/com/example/miniapp/models/Customer.java
@@ -1,4 +1,74 @@
 package com.example.miniapp.models;
 
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+//    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Trip> trips = new ArrayList<>();
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String name, String email, String phoneNumber, List<Trip> trips) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+//        this.trips = trips;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+//    public List<Trip> getTrips() {
+//        return trips;
+//    }
+//
+//    public void setTrips(List<Trip> trips) {
+//        this.trips = trips;
+//    }
 }
+
